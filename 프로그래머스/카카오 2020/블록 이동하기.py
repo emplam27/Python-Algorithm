423304a0b20ca68e90b4dad4eaffb637ac805f8e
emplam27 <emplam27@gmail.com>
2020-08-23 19:56:28 +0900

    0823_Programaners_블록이동하기

--- 프로그래머스/카카오 2020/블록 이동하기.py
+++ 프로그래머스/카카오 2020/블록 이동하기.py
@@ -30,85 +30,10 @@
             위쪽칸 좌하방향 이동[0], 기준의 왼쪽 블럭 검사[1]
 ]
 """
+from collections import deque
 
 
 def solution(board):
-    def move_robot(position, direction, result):
-        nonlocal min_result
-
-        # 최소경우보다 큰 탐색은 걸러주기
-        if result > min_result:
-            return
-
-        # 도착하면 기록 갱신
-        if [n, n] in position:
-            if min_result > result:
-                min_result = result
-            return
-
-        # 평행이동
-        for d in range(4):
-            change_position = [
-                [position[0][0] + moves[0][0][d], position[0][1] + moves[0][1][d]],
-                [position[1][0] + moves[0][0][d], position[1][1] + moves[0][1][d]],
-            ]
-            # 범위안에 존재하고
-            if 0 <= change_position[0][0] < n and 0 <= change_position[0][1] < n \
-                    and 0 <= change_position[1][0] < n and 0 <= change_position[1][1] < n \
-                    and not board[change_position[0][0]][change_position[0][1]] \
-                    and not board[change_position[1][0]][change_position[1][1]]:
-                # 한칸이라도 방문하지 않았다면
-                if not visited[change_position[0][0]][change_position[0][1]] \
-                        or not visited[change_position[1][0]][change_position[1][1]]:
-                    # 가로 방향일 때 상하 이동, 세로 방향일 때 좌우 이동
-                    if (direction == 1 and d == (0 or 2)) or (direction == 2 and d == (1 or 3)):
-                        visited[change_position[0][0]][change_position[0][1]] = 1
-                        visited[change_position[1][0]][change_position[1][1]] = 1
-                        move_robot(change_position, direction, result + 1)
-                        visited[change_position[0][0]][change_position[0][1]] = 0
-                        visited[change_position[1][0]][change_position[1][1]] = 0
-
-                    # 가로 방향일 때 좌우 이동, 세로방향일 때 상하 이동
-                    else:
-                        if change_position[0] in position:
-                            visited[change_position[1][0]][change_position[1][1]] = 1
-                            move_robot(change_position, direction, result + 1)
-                            visited[change_position[1][0]][change_position[1][1]] = 0
-
-                        else:
-                            visited[change_position[0][0]][change_position[0][1]] = 1
-                            move_robot(change_position, direction, result + 1)
-                            visited[change_position[0][0]][change_position[0][1]] = 0
-
-        # 로봇의 방향에 따른 회전이동
-        if direction == 1:
-            change_direction = 2
-        else:
-            change_direction = 1
-
-        for moving, pivot in enumerate([1, 0]):  # 기준점
-            for rotate in [0, 1]:  # 시계방향, 반시계방향
-                # 회전을 위해 비어있어야 할 블럭 검사
-                check_point = [
-                    position[moving][0] + moves[direction][pivot][rotate][1][0],
-                    position[moving][1] + moves[direction][pivot][rotate][1][1]
-                ]
-
-                # 회전
-                if 0 <= check_point[0] < n and 0 <= check_point[1] < n \
-                        and not board[check_point[0]][check_point[1]]\
-                        and not visited[check_point[0]][check_point[1]]:
-                    if pivot == rotate:
-                        change_position = [check_point, position[1]]
-                        visited[check_point[0]][check_point[1]] = 1
-                        move_robot(change_position, change_direction, result + 1)
-                        visited[check_point[0]][check_point[1]] = 0
-                    else:
-                        change_position = [position[0], check_point]
-                        visited[check_point[0]][check_point[1]] = 1
-                        move_robot(change_position, change_direction, result + 1)
-                        visited[check_point[0]][check_point[1]] = 0
-
     n = len(board)
     visited = [[0] * n for _ in range(n)]
     visited[0][0], visited[0][1] = 1, 1
@@ -120,10 +45,18 @@ def solution(board):
         [[[[-1, -1], [0, -1]], [[-1, 1], [0, 1]]], [[[1, 1], [0, 1]], [[1, -1], [0, -1]]]]
     ]
 
-    min_result = n ** 2
-    move_robot([[0, 0], [0, 1]], 1, 0)
+    # [상, 하] or [좌, 우], 방향(상하:1, 좌우: 2),
+    queue, direction = deque([[0, 0], [0, 1]]), 1
+    print(queue)
 
-    return min_result
+    # 방향이 좌우일때
+        # 상하좌우 이동
+        # 좌측좌표 기준 시계, 반시계 방향 회전
+        # 우측좌표 기준 시계, 반시계 방향 회전
+
+    # 방향히 상하일때
+
+    return
 
 
 print(solution([[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]))
