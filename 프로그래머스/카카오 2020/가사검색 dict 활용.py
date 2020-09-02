b85cc8355b2b27876c533cfe70560c7078f3ff29
emplam27 <emplam27@gmail.com>
2020-09-02 22:17:47 +0900

    0902_Programmers_가사검색

--- 프로그래머스/카카오 2020/가사검색 dict 활용.py
+++ 프로그래머스/카카오 2020/가사검색 dict 활용.py
@@ -1,31 +1,57 @@
+"""
+words의 갯수를 trie 구조로 저장
+접두사, 접미사의 두가지 케이스로 나누어 구분
+{'count': 해당_문자_갯수, '문자': 문자별_하위_dict }로 구성
+"""
+
+
 def solution(words, queries):
-    trie_by_length = [({}, {}) for _ in range(10001)]
+    answer = []
+    word_tries = [(dict(), dict()) for _ in range(10001)]
+
+    # trie 구조 만들기
     for word in words:
-        length = len(word)
-        t = trie_by_length[length][0]
-        for c in word:
-            t['count'] = t.get('count', 0) + 1
-            t.setdefault(c, {})
-            t = t[c]
-        t = trie_by_length[length][1]
-        for c in word[::-1]:
-            t['count'] = t.get('count', 0) + 1
-            t.setdefault(c, {})
-            t = t[c]
-    ans = []
+        prefix_trie, suffix_trie = word_tries[len(word)]
+
+        # 접두사 trie
+        cur_prefix_dict = prefix_trie
+        for char in word:
+            cur_prefix_dict['count'] = cur_prefix_dict.get('count', 0) + 1
+            if not cur_prefix_dict.get(char):
+                cur_prefix_dict[char] = dict()
+            cur_prefix_dict = cur_prefix_dict[char]
+
+        # 접미사 trie
+        cur_suffix_dict = suffix_trie
+        for char in word[::-1]:
+            cur_suffix_dict['count'] = cur_suffix_dict.get('count', 0) + 1
+            if not cur_suffix_dict.get(char):
+                cur_suffix_dict[char] = dict()
+            cur_suffix_dict = cur_suffix_dict[char]
+
+    # 가사 검색
     for query in queries:
-        length = len(query)
-        if query[0] == '?':
-            t = trie_by_length[length][1]
+        prefix_trie, suffix_trie = word_tries[len(query)]
+        if query[0] != "?":  # 접두사
+            current_dict = prefix_trie
+        else:  # 접미사
+            current_dict = suffix_trie
             query = query[::-1]
-        else:
-            t = trie_by_length[length][0]
-        for c in query:
-            if c == '?':
-                ans.append(t.get('count', 0))
+        result = current_dict.get('count', 0)  # word에 없는 길이의 query문이 등장할 수 있음
+
+        for char in query:
+            if char == "?":
+                answer.append(result)
                 break
-            if c not in t:
-                ans.append(0)
+            elif not current_dict.get(char):
+                answer.append(0)
                 break
-            t = t[c]
-    return ans
+            else:
+                current_dict = current_dict[char]
+                result = current_dict['count']
+
+    return answer
+
+
+
+
